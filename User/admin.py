53a6a12ababec256be0ed4d252f78a5e0206a150
soroushav <soroushav85@gmail.com>
2023-02-22 23:39:32 +0330

    Update admin.py

--- User/admin.py
+++ User/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
